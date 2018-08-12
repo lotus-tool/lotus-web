1307abd043ac06d63dd72d069805280d973bc93d
Marcos Davy Guedes Dias <m.davy.br@gmail.com>
2018-08-12 00:12:17 -0300

    Create project structure

---
+++ src/main/java/br/uece/gesad/lotus/lotusweb/LotusWebApplication.java
@@ -0,0 +1,12 @@
+package br.uece.gesad.lotus.lotusweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotusWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotusWebApplication.class, args);
+    }
+}
---
+++ src/test/java/br/uece/gesad/lotus/lotusweb/LotusWebApplicationTests.java
@@ -0,0 +1,16 @@
+package br.uece.gesad.lotus.lotusweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LotusWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
