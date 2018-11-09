89f1207292865976865ca96dcc4f907f3f0cc60f
FelipeSVidal <felipedesousavidal@gmail.com>
2018-11-09 12:38:03 -0300

    ADD Microservice -> ModelCheck 	º The function of ModelCheck is check in modeling occurrence Deadlock, Unreacheable and Probability erros.

---
+++ tools/lotus-web-modelCheck/src/test/java/com/gesad/uece/br/lotuswebmodelcheck/LotusWebModelCheckApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gesad.uece.br.lotuswebmodelcheck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LotusWebModelCheckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
